f5555ed483bd53aa7ccd68c836e6eee8a98fd9dc
Ala'aldin Odeh (MatchPoint IT LTD) <v-alaaodeh@microsoft.com>
2025-09-01 09:34:39 +0300

    .

--- backend/telegram-consumer/function_app.py
+++ backend/telegram-consumer/function_app.py
@@ -7,7 +7,7 @@ from consumer import main
 app = func.FunctionApp()
 
 
-@app.timer_trigger(schedule="0 */5 * * * *", arg_name="myTimer", run_on_startup=True, use_monitor=True)
+@app.timer_trigger(schedule="0 */5 * * * *", arg_name="myTimer", run_on_startup=False, use_monitor=True)
 def FetchTelegramData(myTimer: func.TimerRequest) -> None:
 
     if myTimer.past_due:
